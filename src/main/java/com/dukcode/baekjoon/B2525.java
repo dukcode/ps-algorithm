bb81b2ac557f2a732b463f212b8f46eb9a7d9586
김덕윤 <duk9741@gmail.com>
2024-03-23 03:13:47 +0900

    baekjoon: 2525

---
+++ src/main/java/com/dukcode/baekjoon/B2525.java
@@ -0,0 +1,43 @@
+package com.dukcode.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class B2525 {
+
+  private static BufferedReader br;
+  private static BufferedWriter bw;
+  private static StringTokenizer st;
+
+  public static void main(String[] args) throws IOException {
+    br = new BufferedReader(new InputStreamReader(System.in));
+    bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    st = new StringTokenizer(br.readLine());
+    int a = Integer.parseInt(st.nextToken());
+    int b = Integer.parseInt(st.nextToken());
+    int c = Integer.parseInt(br.readLine());
+
+    bw.write(solve(a, b, c));
+
+    br.close();
+    bw.close();
+  }
+
+  private static String solve(int a, int b, int c) {
+    int hour = a;
+    int min = b + c;
+
+    hour += min / 60;
+    min %= 60;
+
+    hour %= 24;
+
+    return String.format("%d %d", hour, min);
+  }
+
+}
