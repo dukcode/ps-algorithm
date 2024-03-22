8c8b8d948dd399fec8b8232b12760147fbe7b8a0
김덕윤 <duk9741@gmail.com>
2024-03-23 03:52:46 +0900

    baekjoon: 9653

---
+++ src/main/java/com/dukcode/baekjoon/B9653.java
@@ -0,0 +1,33 @@
+package com.dukcode.baekjoon;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class B9653 {
+
+  private static BufferedWriter bw;
+
+  public static void main(String[] args) throws IOException {
+    bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    bw.write(
+        "    8888888888  888    88888\n"
+            + "   88     88   88 88   88  88\n"
+            + "    8888  88  88   88  88888\n"
+            + "       88 88 888888888 88   88\n"
+            + "88888888  88 88     88 88    888888\n"
+            + "\n"
+            + "88  88  88   888    88888    888888\n"
+            + "88  88  88  88 88   88  88  88\n"
+            + "88 8888 88 88   88  88888    8888\n"
+            + " 888  888 888888888 88  88      88\n"
+            + "  88  88  88     88 88   88888888"
+    );
+
+    bw.close();
+
+  }
+
+
+}
