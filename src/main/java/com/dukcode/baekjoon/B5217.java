7e8d79e65cd3a51bed001d5d0065f60013e88547
김덕윤 <duk9741@gmail.com>
2024-03-23 03:45:10 +0900

    baekjoon: 5217

---
+++ src/main/java/com/dukcode/baekjoon/B5217.java
@@ -0,0 +1,42 @@
+package com.dukcode.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringJoiner;
+
+public class B5217 {
+
+  private static BufferedReader br;
+  private static BufferedWriter bw;
+
+  public static void main(String[] args) throws IOException {
+    br = new BufferedReader(new InputStreamReader(System.in));
+    bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    int numCase = Integer.parseInt(br.readLine());
+    while (numCase-- > 0) {
+      int n = Integer.parseInt(br.readLine());
+
+      int half = (n + 1) / 2;
+
+      StringJoiner ans = new StringJoiner(", ");
+      for (int first = 1; first < half; ++first) {
+        int second = n - first;
+        ans.add(String.format("%d %d", first, second));
+      }
+
+      bw.write(String.format("Pairs for %d: ", n));
+      bw.write(ans.toString());
+      bw.newLine();
+    }
+
+    br.close();
+    bw.close();
+
+  }
+
+
+}
