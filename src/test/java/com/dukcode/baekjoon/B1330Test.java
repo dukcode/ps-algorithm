27ab3f8dbd7b1ef2b094d902e8e519a231bc123e
김덕윤 <duk9741@gmail.com>
2024-03-23 02:57:31 +0900

    baekjoon: 1330

---
+++ src/test/java/com/dukcode/baekjoon/B1330Test.java
@@ -0,0 +1,28 @@
+package com.dukcode.baekjoon;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.params.provider.Arguments;
+import util.ProblemSolvingTest;
+
+class B1330Test extends ProblemSolvingTest {
+
+  public B1330Test() {
+    super(3);
+  }
+
+  public static Stream<Arguments> setData() {
+    return Stream.of(
+        Arguments.of("1 2", "<"),
+        Arguments.of("10 2", ">"),
+        Arguments.of("-10000 -10000", "=="),
+        Arguments.of("10000 10000", "=="),
+        Arguments.of("5 5", "==")
+    );
+  }
+
+  @Override
+  protected void runMain() throws Exception {
+    B1330.main(new String[]{});
+  }
+
+}
