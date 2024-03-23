81170995ec014d8e88d23b44ac466e3610194179
김덕윤 <duk9741@gmail.com>
2024-03-23 18:14:56 +0900

    baekjoon: 10807

---
+++ src/main/java/com/dukcode/baekjoon/day240323/B10807.java
@@ -0,0 +1,40 @@
+package com.dukcode.baekjoon.day240323;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class B10807 {
+
+  private static final int MIN = -100;
+  private static final int MAX = 100;
+  private static BufferedReader br;
+  private static BufferedWriter bw;
+  private static StringTokenizer st;
+
+  public static void main(String[] args) throws IOException {
+    br = new BufferedReader(new InputStreamReader(System.in));
+    bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    int n = Integer.parseInt(br.readLine());
+
+    int[] freq = new int[MAX - MIN + 1];
+    st = new StringTokenizer(br.readLine());
+    for (int i = 0; i < n; ++i) {
+      int num = Integer.parseInt(st.nextToken());
+      freq[num - MIN]++; // ?
+    }
+
+    int v = Integer.parseInt(br.readLine());
+
+    bw.write(String.valueOf(freq[v - MIN]));
+
+    br.close();
+    bw.close();
+
+  }
+
+}
