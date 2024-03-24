b03d23727da27b355a9419a5cf1abe93ac5abd73
김덕윤 <duk9741@gmail.com>
2024-03-24 20:56:48 +0900

    baekjoon: 1919

---
+++ src/test/java/com/dukcode/baekjoon/day240324/B1919Test.java
@@ -0,0 +1,55 @@
+package com.dukcode.baekjoon.day240324;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.params.provider.Arguments;
+import util.ProblemSolvingTest;
+
+class B1919Test extends ProblemSolvingTest {
+
+  public B1919Test() {
+    super(3);
+  }
+
+  public static Stream<Arguments> setData() {
+    return Stream.of(
+        Arguments.of(
+            """
+                aabbcc
+                xxyybb
+                """,
+            """
+                8
+                """),
+        Arguments.of(
+            """
+                bbbbbb
+                aaaaaa
+                """,
+            """
+                12
+                """),
+        Arguments.of(
+            """
+                a
+                a
+                """,
+            """
+                0
+                """),
+        Arguments.of(
+            """
+                a
+                b
+                """,
+            """
+                2
+                """)
+    );
+  }
+
+  @Override
+  protected void runMain() throws Exception {
+    B1919.main(new String[]{});
+  }
+
+}
