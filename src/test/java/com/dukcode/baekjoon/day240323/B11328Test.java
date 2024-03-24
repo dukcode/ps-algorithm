fd51aa27736b690b73f3ddd7e0876213a10bb0a5
김덕윤 <duk9741@gmail.com>
2024-03-24 20:52:16 +0900

    baekjoon: 11328

---
+++ src/test/java/com/dukcode/baekjoon/day240323/B11328Test.java
@@ -0,0 +1,40 @@
+package com.dukcode.baekjoon.day240323;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.params.provider.Arguments;
+import util.ProblemSolvingTest;
+
+class B11328Test extends ProblemSolvingTest {
+
+  public B11328Test() {
+    super(3);
+  }
+
+  public static Stream<Arguments> setData() {
+    return Stream.of(
+        Arguments.of(
+            """
+                5
+                a a
+                ab ba
+                ring gnir
+                newt twan
+                aaaa aaaa
+                """,
+            """
+                Possible
+                Possible
+                Possible
+                Impossible
+                Possible
+                """)
+
+    );
+  }
+
+  @Override
+  protected void runMain() throws Exception {
+    B11328.main(new String[]{});
+  }
+
+}
